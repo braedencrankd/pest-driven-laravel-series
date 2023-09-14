fcf1697c28f3f8ded60b0525839478f71fe613e2
braedencrankd <braeden@crankd.nz>
2023-09-15 09:17:56 +1200

    Added xdebug profiling

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,19 +1,9 @@
 <?php
 
-namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+it('gives back successful response for homepage', function () {
+    $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
-}
+    $response->assertStatus(200);
+});
